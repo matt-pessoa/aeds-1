3be09a620d40cf4d263f141bae498e06f1884e31
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2022-04-06 07:53:59 -0300

    feat: exercicio 2 - adiciona verificação para encontrar o maior e o menor número digitado

--- lista03/exercicio2.c
+++ lista03/exercicio2.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
-#include <math.h>
 
 int main()
 {
-  int contador = 0;
+  int contador = 0, maior, menor;
   float num, soma = 0, media;
 
   printf("Digite um número inteiro ou -1 para finalizar: ");
   scanf("%f", &num);
 
+  maior = num;
+  menor = num;
+
   while (num != -1)
   {
     soma += num;
     contador += 1;
+
+    if (num > maior)
+    {
+      maior = num;
+    }
+    if (num < menor)
+    {
+      menor = num;
+    }
 
     printf("Digite um número inteiro ou -1 para finalizar: ");
     scanf("%f", &num);
@@ -20,5 +31,5 @@ int main()
 
   media = soma / contador;
 
-  printf("\nA média é %f", media);
+  printf("\nA média é %f \nO maior número é %d \nO menor número é %d", media, maior, menor);
 }
